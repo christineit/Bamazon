eb9581b8bb8cf999f641c3ec5bf01800570420cd
Christine <christineitrinh@gmail.com>
2019-04-03 16:51:41 -0500

    trying to get logic of order placing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,7 +28,7 @@ function showProducts() {
     if (err) throw err;
     // console.log(res);
     var showTable = new Table({
-      head: ["Item ID", "Product Name", "Department", "Price", "In Stock"],
+      head: ["Product ID", "Product Name", "Department", "Price", "In Stock"],
       colWidths: [10, 15, 20, 20, 10]
     });
     for (var i = 0; i < res.length; i++) {
@@ -43,14 +43,46 @@ function showProducts() {
     // console.log(res[0]);
     // console.log([res[0]]); //trying to get into this object.  Why need array? around [res]?
     console.log(showTable.toString());
+    shoppingPrompt();
   });
 }
 
-//connection.end();
+function shoppingPrompt() {
+  inquirer
+    .prompt([
+      {
+        name: "productId",
+        type: "input",
+        message: "What is the Product ID?"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many items do you want to purchase??"
+      }
+    ])
+    .then(function(ans) {
+      //console.log(ans);
+      //store ans in array?
+      var productID = ans.productId;
+      var productNeeded = ans.quantity;
 
-/* function showProduct() {
-    inquirer.prompt({
-        name: 
-    })
+      placeOrder();
+        }
+      );
+    
+
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
+function placeOrder(){
+
 }
- */
+// //if else statement:
+//     // if qty id(user) <= available(DB) quantity {
+//       update database
+//       console.log(Thank you for shopping)
+//       showProducts()
+//   }else {console.log(Insufficient qty, please buy something else)
